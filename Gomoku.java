d1c98dfb7a7631002223711b1faa06f274f5be74
Ryan Rose <rytrose@gmail.com>
2014-03-25 11:37:56 -0400

    added isOpen

--- Gomoku.java
+++ Gomoku.java
@@ -1,11 +1,12 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.Color;
 /** 
  * The game Gomoku, which is a two player game played on a grid,
  * with the winner being the first player to get five of their pieces in a row either
  * horizontally, vertically, or diagonally.
  * @author Ryan Rose
  */
-import javax.swing.JFrame;
-import javax.swing.JButton;
 public class Gomoku extends JFrame{
   // Number of rows of the game.
   private int rows = 0;
@@ -90,27 +91,112 @@ public class Gomoku extends JFrame{
     else
       return 0;
   }
+  
+  /** 
+   * Returns true if the space in an input direction starting from the piece being played is open
+   * at the end of all the played pieces of the color that's being played.
+   * @param board  two dimensional array of JButtons used as the board
+   * @param row  row of currently played piece
+   * @param column  column of currently played piece
+   * @param direction  specified direction from currently played piece of either 
+   *        "N", "NE", "E", "SE", "S", "SW", "W", or "NW"
+   * @return returns true if the space at the end of the currently played pieces of the color being
+   *         played in the specified direction is open
+   */
+  public boolean isOpen(JButton[][] board, int row, int column, String direction){
+    Color green = Color.green;
+    // Handles north.
+    if(direction == "N"){
+      int i = 0;
+      while(row - i >= 0 &&
+            (board[row - i][column].getBackground() == board[row][column].getBackground()))
+        i++;
+      if(board[row - i][column].getBackground() == green)
+        return true;
+      else
+        return false;
+    }
+    // Handles northeast.
+    if(direction == "NE"){
+      int i = 0;
+    //  Color green = new Color
+      while((row - i >= 0) && (column + i < board[0].length) && 
+            (board[row - i][column + i].getBackground() == board[row][column].getBackground()))
+        i++;
+      if(board[row - i][column + i].getBackground() == green)
+        return true;
+      else
+        return false;
+    }
+    // Handles east.
+    if(direction == "E"){
+      int i = 0;
+      while((column + i < board[0].length) &&
+            (board[row][column + i].getBackground() == board[row][column].getBackground()))
+        i++;
+      if(board[row][column + i].getBackground() == green)
+        return true;
+      else
+        return false;
+    }
+    // Handles southeast.
+    if(direction == "SE"){
+      int i = 0;
+      while((row + i < board.length) && (column + i < board[0].length) &&
+            (board[row + 1][column + 1].getBackground() == board[row][column].getBackground()))
+        i++;
+      if(board[row + 1][column + 1].getBackground() == green)
+        return true;
+      else
+        return false;
+    }
+    // Handles south.
+    if(direction == "S"){
+      int i = 0;
+      while(row + i < board.length &&
+            (board[row + i][column].getBackground() == board[row][column].getBackground()))
+        i++;
+      if(board[row + i][column].getBackground() == green)
+        return true;
+      else
+        return false;
+    }
+    // Handles southwest.
+    if(direction == "SW"){
+      int i = 0;
+      while((row + i < board.length) && (column - i >= 0) &&
+            (board[row + i][column].getBackground() == board[row][column].getBackground()))
+        i++;
+      if(board[row + i][column].getBackground() == green)
+        return true;
+      else
+        return false;
+    }
+    // Handles west.
+    if(direction == "W"){
+      int i = 0;
+      while(column - i >= 0 &&
+            (board[row][column - i].getBackground() == board[row][column].getBackground()))
+        i++;
+      if(board[row][column - i].getBackground() == green)
+        return true;
+      else
+        return false;
+    }
+    // Handels northwest.
+    if(direction == "NW"){
+      int i = 0;
+      while((row - i >= 0) && (column - i >= 0) &&
+            (board[row - i][column - i].getBackground() == board[row][column].getBackground()))
+        i++;
+      if(board[row - i][column - i].getBackground() == green)
+        return true;
+      else
+        return false;
+    }
+    else 
+      return false;
+  }
 }
 
-/** 
- * Returns true if the space in an input direction starting from the piece being played is open
- * at the end of all the played pieces of the color that's being played.
- * @param board  two dimensional array of JButtons used as the board
- * @param row  row of currently played piece
- * @param column  column of currently played piece
- * @param direction  specified direction from currently played piece of either 
- *        "N", "NE", "E", "SE", "S", "SW", "W", or "NW"
- * @return returns true if the space at the end of the currently played pieces of the color being
- *         played in the specified direction is open
- */
-public boolean isOpen(JButton[][] board, int row, int column, String direction){
- // Handles north.
- if(direction == "N"){
- int i;
- for(i = 0; row - i >= 0; i++){
- if(board[row - i][column].getBackground() == board[row][column].getBackground()){}          
- }
- return inLine;
- }
- }
- }
+  
